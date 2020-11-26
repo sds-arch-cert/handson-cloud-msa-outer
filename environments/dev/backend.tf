69e505a45fc55a72c222d278ac5251fc64cb997c
gwanghui <park776139@gmail.com>
2020-11-26 17:39:11 +0900

    add

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "architect-certification-289902-MEMBER_ID-tfstate"
+    bucket = "architect-certification-289902-43-tfstate"
     prefix = "environments/dev"
   }
 }
